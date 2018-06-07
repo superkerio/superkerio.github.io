49150e2754907002c5a49de86e52cfc12fc1f338
xianxiaobo <1015207497@qq.com>
2018-06-07 16:23:31 +0800

    搭建结构

--- Angular-Admin/src/app/main/main.module.ts
+++ Angular-Admin/src/app/main/main.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from "./main.component";
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MainRoutingModule
   ],
-  declarations: [MainComponent]
+  declarations: [MainComponent, SidebarComponent, HeaderComponent, FooterComponent]
 })
 export class MainModule { }
