f3ee7a98c58ed0ae820a1598be86816dc864e92d
ilovekerry <xxb2015@gmail.com>
2020-01-10 10:00:16 +0800

    去掉了Ueditor，重新打包

--- Angular-Admin/src/app/main/content/editor/editor-routing.module.ts
+++ Angular-Admin/src/app/main/content/editor/editor-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {UeditorComponent} from "./ueditor/ueditor.component";
+// import {UeditorComponent} from "./ueditor/ueditor.component";
 import {CkeditorComponent} from "./ckeditor/ckeditor.component";
 import { TinymceComponent } from './tinymce/tinymce.component';
 
 const routes: Routes = [
-  {
-    path: "ueditor",
-    component: UeditorComponent
-  },
+  // {
+  //   path: "ueditor",
+  //   component: UeditorComponent
+  // },
   {
     path: "ckeditor",
     component: CkeditorComponent
--- Angular-Admin/src/app/main/content/editor/editor.module.ts
+++ Angular-Admin/src/app/main/content/editor/editor.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ComponentModule } from "../../../common/module/component.module";
 import { EditorRoutingModule } from './editor-routing.module';
-import { UEditorModule } from "ngx-ueditor";
 import { CKEditorModule } from "ng2-ckeditor";
 import {SharedModule} from "../../../common/module/shared.module";
 
-import {UeditorComponent} from "./ueditor/ueditor.component";
 import {CkeditorComponent} from "./ckeditor/ckeditor.component";
 import {TinymceComponent} from './tinymce/tinymce.component';
 
@@ -15,14 +13,8 @@ import {TinymceComponent} from './tinymce/tinymce.component';
     SharedModule,
     ComponentModule,
     EditorRoutingModule,
-    UEditorModule.forRoot({
-      path: 'assets/ueditor/',
-      options: {
-        themePath: (location.href.indexOf('github') !== -1 ? '/Angular-Admin/dist' : '.') + '/assets/ueditor/themes/'
-      },
-    }),
     CKEditorModule
   ],
-  declarations: [UeditorComponent, CkeditorComponent, TinymceComponent],
+  declarations: [CkeditorComponent, TinymceComponent],
 })
 export class EditorModule { }
